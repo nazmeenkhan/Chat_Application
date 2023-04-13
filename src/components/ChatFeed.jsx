8fd861158a42420eea68d05fd28e4da28f021dc5
Nazmeen Khan <nazmeen.khan@infosys.com>
2023-04-13 10:05:37 +0530

    basic skeleton for chat feed

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -8,14 +8,42 @@ const Chatfeed = (props) => {
   const chat = chats && chats[activeChat];
   const renderMessages = () => {
     const keys = Object.keys(messages)
-    return keys.map(() => {
-      
+    return keys.map((key, index) => {
+      const message = messages[key]
+      const lastMessageKey = index === 0 ? null : keys[index-1];
+      const isMyMessage = userName === message.sender.username;
+      return (
+        <div key={`msg_${index}`} style={{ width: "100%" }}>
+          <div className="message-block">
+            {
+              isMyMessage 
+              ? <MyMessage message={message} /> 
+              : <OtherMessage message={message} lastMessage={messages[lastMessageKey]} />
+            }
+          </div>
+          <div className="" style={{}}>
+            read-reciept
+          </div>
+        </div>
+      )
     })
   }
 
+  if(!chat) return 'Loading...'
   return(
-    <div>
-      ChatFeed
+    <div className="chat-title-container">
+      <div className="chat-title">
+        {chat?.title}
+      </div>
+      <div className="chat-subtitle"> 
+        {
+          chat?.people.map((person) => `${person.person.username}`)
+        }
+      </div> 
+      {renderMessages()}
+      <div className="message-form-container">
+        <MessageForm {...props} chatId={activeChat} />
+      </div>
     </div>
   )
 }
--- src/components/OtherMessage.jsx
+++ src/components/OtherMessage.jsx
@@ -1,7 +1,23 @@
-const OtherMessage = () => {
+const OtherMessage = ({ message, lastMessage }) => {
+  const isFirstMessage = !lastMessage || lastMessage.sender.username !== message.sender.username
+  
   return(
     <div>
-      OtherMessage
+      {isFirstMessage && (
+        <div />
+      )}
+      {
+        message?.attachments?.length > 0
+        ? (
+          <img src={message.attachments[0].file} 
+            alt="message-attachment"
+            className="message-image"
+            style={{float: 'left'}}
+          />
+        ) : (
+          <div className="message-bubble" style={{float: 'left'}}> {message.text} </div>
+        )
+      }
     </div>
   )
 }
